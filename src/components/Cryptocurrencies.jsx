186f7204522560b372a055921545187e334b3a12
Mesfin Girma <mesfingirmatwo@gmail.com>
2022-05-28 01:04:17 +0300

    coin-list-design

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = () => {
+    const {data: cryptosList, isFetching} = useGetCryptosQuery(10);
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+    console.log(cryptos);
   return (
     <div>Cryptocurrencies</div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,14 @@ import millify from 'millify'
 import { Typography, Row,Col, Statistic } from 'antd'
 import { Link } from 'react-router-dom'
 import { useGetCryptosQuery  } from '../services/cryptoApi'
+import Cryptocurrencies from './Cryptocurrencies'
+import News from './News'
 const {Title} = Typography;
 const Home = () => {
     const {data, isFetching} = useGetCryptosQuery (10);
     const globalStats = data?.data?.stats;
     if(isFetching) return 'Loading......'
-    console.log(data);
+  
   return (
     <>
        <Title level={2} className='heading'> Global Crypto Stats</Title>
@@ -20,6 +22,16 @@ const Home = () => {
            <Col span={12}> <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}></Statistic></Col>
 
        </Row>
+       <div className="home-heading-container">
+           <Title level={2} className='home-title'>Top 10 Cryptocurrencies in the world</Title>
+           <Title level={3} className= "show-more"> <Link to="/cryptocurrencies">Show More</Link></Title>
+       </div>
+       <Cryptocurrencies simplified/>
+       <div className="home-heading-container">
+           <Title level={2} className='home-title'>Latest Crypto News</Title>
+           <Title level={3} className= "show-more"> <Link to="/cryptocurrencies">Show More</Link></Title>
+       </div>
+       <News simplified />
     </>
   )
 }
