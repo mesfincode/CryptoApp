2485b181fc89aee065eaa94b2acb18282a2ec8f7
Mesfin Girma <mesfingirmatwo@gmail.com>
2022-05-28 01:36:28 +0300

    coin-card-designed

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,12 +1,37 @@
 import React,{useState} from 'react'
 import { useGetCryptosQuery } from '../services/cryptoApi'
+import { Typography, Row,Col, Statistic,Card } from 'antd'
+import { Link } from 'react-router-dom'
+import millify from 'millify'
+
 
 const Cryptocurrencies = () => {
     const {data: cryptosList, isFetching} = useGetCryptosQuery(10);
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
     console.log(cryptos);
   return (
-    <div>Cryptocurrencies</div>
+    <> 
+      <Row gutters={[32,32]} className="crypto-card-container">
+           { cryptos.map((currency)=>(
+               <Col xs={24} sm={12} lg={6} className="crypto-card" key ={currency.id}>
+                   <Link to={`/crypto/${currency.id}`}>
+                        <Card 
+                           title={`${currency.rank}. ${currency.name}`}
+                           extra={<img className="crypto-image" src={currency.iconUrl}/>}
+                        >
+                         <p>Price: {millify(currency.price)}</p>
+                         <p>Market Cap: {millify(currency.marketCap)}</p>
+
+                         <p>Daily change: {millify(currency.change)}%</p>
+
+                        </Card>
+                   </Link>
+               </Col>
+           ))
+
+           }
+      </Row>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
  import 'antd/dist/antd.variable.min.css';
  import store from './app/store'
  import {Provider} from 'react-redux'
+//  import 'bootstrap/dist/css/bootstrap.min.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <Router>
