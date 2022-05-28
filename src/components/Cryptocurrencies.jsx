785b774349969e03310bf836859d1ffe36a85f02
Mesfin Girma <mesfingirmatwo@gmail.com>
2022-05-28 21:01:54 +0300

    Show more desgn completed

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,14 +5,16 @@ import { Link } from 'react-router-dom'
 import millify from 'millify'
 
 
-const Cryptocurrencies = () => {
-    const {data: cryptosList, isFetching} = useGetCryptosQuery(10);
+const Cryptocurrencies = ({simplified}) => {
+  const count= simplified ? 10: 100;
+    const {data: cryptosList, isFetching} = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
     console.log(cryptos);
+    if(isFetching) return 'Loading......'
   return (
     <> 
-      <Row gutters={[32,32]} className="crypto-card-container">
-           { cryptos.map((currency)=>(
+      <Row gutter={[32,32]} className="crypto-card-container">
+           { cryptos?.map((currency)=>(
                <Col xs={24} sm={12} lg={6} className="crypto-card" key ={currency.id}>
                    <Link to={`/crypto/${currency.id}`}>
                         <Card 
